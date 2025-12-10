eb827d755699e09ee175031f3596e457c44c5bfe
YouSungMin <xormraoghk@gmail.com>
2025-12-10 16:53:41 +0900

    1210_EnemyDataStructs 구조체 생성(구현x)

--- Source/MegaBonkClone/Private/Data/EnemyDataStructs.cpp
+++ Source/MegaBonkClone/Private/Data/EnemyDataStructs.cpp
@@ -1,12 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "EnemyDataStructs.h"
-
-EnemyDataStructs::EnemyDataStructs()
-{
-}
-
-EnemyDataStructs::~EnemyDataStructs()
-{
-}
+#include "Data/EnemyDataStructs.h"
