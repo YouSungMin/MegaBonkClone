b7161c0768858f280d9c50b578131b636e2d424c
YouSungMin <xormraoghk@gmail.com>
2025-12-10 14:40:43 +0900

    1210_프로젝트 생성 커밋

---
+++ Source/MegaBonkClone/MegaBonkClone.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MegaBonkClone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MegaBonkClone, "MegaBonkClone" );
