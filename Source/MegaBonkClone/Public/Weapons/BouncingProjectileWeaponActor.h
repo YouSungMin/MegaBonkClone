d82a5e7055926b87e63dbad0f5641bc84031f93a
KimSungHyun <144310761+SUNMUNG@users.noreply.github.com>
2025-12-15 17:35:26 +0900

    1215 - 투사체형 무기 바리에이션 추가 (튕기는 투사체)

---
+++ Source/MegaBonkClone/Public/Weapons/BouncingProjectileWeaponActor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/ProjectileWeaponActor.h"
+#include "BouncingProjectileWeaponActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGABONKCLONE_API ABouncingProjectileWeaponActor : public AProjectileWeaponActor
+{
+	GENERATED_BODY()
+	
+public:
+	// 초기화 함수 확장 (바운스 횟수, 탐색 범위 추가)
+	void InitializeBouncing(int32 InBounces, float InBounceRange);
+
+protected:
+	virtual void OnProjectileHit(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	// 다음 타겟 찾는 헬퍼 함수
+	AActor* FindNextTarget(const FVector& Origin);
+
+private:
+	int32 BouncesLeft = 5;   // 남은 튕김 횟수
+	float BounceRange = 1000.0f; // 튕길 때 다음 적 찾는 범위
+
+	// 이미 맞춘 적은 다시 안 맞추기 위해 기록
+	UPROPERTY()
+	TArray<AActor*> HitHistory;
+};
