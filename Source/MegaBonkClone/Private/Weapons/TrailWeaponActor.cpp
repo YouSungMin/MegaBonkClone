9f251f40761fe7d13d3b715ece96d07fa79c1366
KimSungHyun <144310761+SUNMUNG@users.noreply.github.com>
2025-12-16 11:52:25 +0900

    1216 - Weapon ( 불꽃 걸음 ) 변경

--- Source/MegaBonkClone/Private/Weapons/TrailWeaponActor.cpp
+++ Source/MegaBonkClone/Private/Weapons/TrailWeaponActor.cpp
@@ -4,13 +4,14 @@
 #include "Weapons/TrailWeaponActor.h"
 #include "NiagaraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Paper2D/Classes/PaperSpriteComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ATrailWeaponActor::ATrailWeaponActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
 	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	SetRootComponent(Root);
@@ -21,8 +22,20 @@ ATrailWeaponActor::ATrailWeaponActor()
 	OverlapComp->SetCapsuleRadius(100.0f);
 	OverlapComp->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
 
-	EffectComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Effect"));
-	EffectComp->SetupAttachment(Root);
+	SpriteComp = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("SpriteComp"));
+	SpriteComp->SetupAttachment(Root);
+	SpriteComp->SetRelativeRotation(FRotator(0.0f, 0.0f, 90.0f));
+	SpriteComp->SetSpriteColor(FLinearColor(1.0f, 0.3f, 0.05f, 0.85f));
+	SpriteComp->SetRelativeScale3D(FVector(3.0f, 3.0f, 3.0f));
+	SpriteComp->SetCollisionProfileName("NoCollision");
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(SpriteComp);
+	MeshComp->SetCollisionProfileName("NoCollision");
+	MeshComp->SetRelativeRotation(FRotator(0.0f, 0.0f, 90.0f));
+	MeshComp->SetRelativeScale3D(FVector(0.19f, 0.19f, 0.01f));
+
+	ScaleTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("ScaleTimeline"));
 
 }
 
@@ -44,6 +57,17 @@ void ATrailWeaponActor::InitializeTrail(float InDamage, float InDuration, float 
 		AttackSpeed,
 		true
 	);
+
+	InitialScale = FVector(InScale);
+	SetActorScale3D(InitialScale);
+
+	// [핵심] 전체 수명에서 '커브 길이(0.5초)'를 뺀 시간에 타이머를 겁니다.
+	// 예: 5초 지속이면, 4.5초 뒤에 작아지기 시작.
+	float CurveDuration = 0.5f; // 혹은 ShrinkCurve->FloatCurve.GetLastKey().Time;
+	float StartTime = FMath::Max(0.0f, InDuration - CurveDuration);
+
+	FTimerHandle Handle;
+	GetWorldTimerManager().SetTimer(Handle, this, &ATrailWeaponActor::StartShrink, StartTime, false);
 }
 
 
@@ -69,5 +93,42 @@ void ATrailWeaponActor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// [중요] 타임라인과 함수 연결 (바인딩)
+	if (ScaleCurve)
+	{
+		// 1. 업데이트 함수 연결 (매 프레임 호출)
+		UpdateFunctionFloat.BindUFunction(this, FName("OnShrinkUpdate"));
+		// 2. 종료 함수 연결 (끝나면 호출)
+		FinishedFunctionEvent.BindUFunction(this, FName("OnShrinkFinished"));
+
+		// 3. 타임라인에 등록
+		ScaleTimeline->AddInterpFloat(ScaleCurve, UpdateFunctionFloat);
+		ScaleTimeline->SetTimelineFinishedFunc(FinishedFunctionEvent);
+
+		// 루프 안 함
+		ScaleTimeline->SetLooping(false);
+	}
+
+}
+
+void ATrailWeaponActor::OnShrinkUpdate(float Value)
+{
+	// 원래 크기 * 커브 값
+	SetActorScale3D(InitialScale * Value);
+}
+
+void ATrailWeaponActor::OnShrinkFinished()
+{
+	Destroy();
+}
+
+void ATrailWeaponActor::StartShrink()
+{
+	// 타임라인 재생!
+	if (ScaleTimeline)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("StartShrink"));
+		ScaleTimeline->PlayFromStart();
+	}
 }
 
