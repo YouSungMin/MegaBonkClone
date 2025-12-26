621e619ba8d1561bad361e452e85efb014cc07e9
kdjh2002 <187482672+kdjh2002@users.noreply.github.com>
2025-12-26 12:13:34 +0900

    12/26_데미지팝업

---
+++ Source/MegaBonkClone/Public/UI/DamageValueWidget.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DamageValueWidget.generated.h"
+
+class UTextBlock;
+class UWidgetAnimation;
+/**
+ * 
+ */
+UCLASS()
+class MEGABONKCLONE_API UDamageValueWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void PlayDamagePopup(float Damage);
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> DamageText = nullptr;;
+
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+	TObjectPtr<UWidgetAnimation> DamagePopupAnimation = nullptr;
+
+};
